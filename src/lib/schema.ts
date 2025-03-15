9c5096a4a49aa493c89752e3cd889cc9c0855a1d
Peter Indiola <pindiola@gmail.com>
2025-03-15 12:04:58 +0800

    feat: added db schema

---
+++ src/lib/schema.ts
@@ -0,0 +1,25 @@
+import { Generated, GeneratedAlways } from 'kysely'
+
+export interface Database {
+  documents: DocumentTable
+}
+
+export interface DocumentTable {
+  id: GeneratedAlways<number>
+  name: string
+  type: string
+  size: number
+  folder_id: number | null
+  created_by: string
+  created_at: Generated<Date>
+  updated_at: Generated<Date>
+}
+
+export interface FolderTable {
+  id: GeneratedAlways<number>
+  name: string
+  parent_id: number | null
+  created_by: string
+  created_at: Generated<Date>
+  updated_at: Generated<Date>
+}
