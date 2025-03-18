9563dd306f924b98bf535b100eb0beb77078c5b4
Peter Indiola <pindiola@gmail.com>
2025-03-18 22:56:56 +0800

    chore: fix docker node modules issues and change port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import app from './app'
 
 dotenv.config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
